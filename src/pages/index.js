0c1b5e36720fb4922a601ee1874d3871b995ce04
yugasun <yuga_sun@163.com>
2020-05-23 18:45:27 +0800

    fix: home page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,9 +70,7 @@ function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title="Home" description={`${siteConfig.tagline}`}>
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
